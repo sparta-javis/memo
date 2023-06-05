74b08563e8311534bf1722f21209c49cf6af1e7d
iseungchul <tnsrb950@gmail.com>
2023-06-05 12:18:20 +0900

    seungchul

--- Memo.java
+++ Memo.java
@@ -2,7 +2,7 @@ package team.memo;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-//이건 이승철 브랜치입니다 브랜치입니다.sdfsfafsf
+//이건 이승철 브랜치입니다 브랜치입니다.sdfsfafsfdfss dfsf
 class Memo {
     String name;
     private int password;
